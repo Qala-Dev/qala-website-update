02e806c58e92cce675769f78ae2846cb1246b5ab
Oluwatobi Bamidele <teebams49@gmail.com>
2022-12-06 07:29:36 +0000

    update: added links to the footer social media icons

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -9,15 +9,21 @@ export default function Footer() {
       <div className=" h-1 w-24 bg-nav-blue rounded dark:bg-nav-white"></div>
       <div className="flex flex-col items-center justify-center mt-8">
         <div className="flex">
-          <div className=" h-8 w-8 rounded-[5px] flex items-center justify-center bg-facebook-blue mx-2">
-            <Image src={Facebook} alt="Facebook" />
-          </div>
-          <div className=" h-8 w-8 rounded-[5px] flex items-center justify-center bg-twitter-blue mx-2">
-            <Image src={Twitter} alt="Twitter" />
-          </div>
-          <div className=" h-8 w-8 rounded-[5px] flex items-center justify-center bg-linkedin-blue mx-2">
-            <Image src={LinkedIn} alt="LinkedIn" />
-          </div>
+          <a href="">
+            <div className=" h-8 w-8 rounded-[5px] flex items-center justify-center bg-facebook-blue mx-2">
+              <Image src={Facebook} alt="Facebook" />
+            </div>
+          </a>
+          <a href="https://twitter.com/QalaAfrica">
+            <div className=" h-8 w-8 rounded-[5px] flex items-center justify-center bg-twitter-blue mx-2">
+              <Image src={Twitter} alt="Twitter" />
+            </div>
+          </a>
+          <a href="https://www.linkedin.com/company/qala-africa/">
+            <div className=" h-8 w-8 rounded-[5px] flex items-center justify-center bg-linkedin-blue mx-2">
+              <Image src={LinkedIn} alt="LinkedIn" />
+            </div>
+          </a>
         </div>
         <p className="mt-4 font-normal text-base text-nav-blue dark:text-nav-white">
           Copyright © Qala 2022
