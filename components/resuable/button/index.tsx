6c07fb62b625c3680ed25c3540a7f13f06f32fe8
Oluwatobi Bamidele <teebams49@gmail.com>
2022-12-18 23:29:49 +0100

    added hover state to button component

--- components/resuable/button/index.tsx
+++ components/resuable/button/index.tsx
@@ -1,5 +1,5 @@
-import Link from "next/link";
 import { ReactNode } from "react";
+import { motion } from "framer-motion";
 
 interface props {
   children: ReactNode;
@@ -9,15 +9,25 @@ interface props {
 
 export default function Button({ children, href, newTab }: props) {
   const className =
-    "py-[0.875rem] lg:py-[1.125rem] bg-nav-blue rounded w-full text-lg text-nav-white dark:bg-nav-white dark:text-nav-blue";
+    "py-[0.875rem] lg:py-[1.125rem] bg-nav-blue rounded w-full text-lg text-nav-white dark:bg-nav-white dark:text-nav-blue hover:dark:text-nav-white";
   return (
     <>
       {href ? (
         <a href={href}>
-          <button className={className}>{children}</button>
+          <motion.button
+            whileHover={{ backgroundColor: "#4665C0" }}
+            className={className}
+          >
+            {children}
+          </motion.button>
         </a>
       ) : (
-        <button className={className}>{children}</button>
+        <motion.button
+          whileHover={{ backgroundColor: "#4665C0" }}
+          className={className}
+        >
+          {children}
+        </motion.button>
       )}
     </>
   );
