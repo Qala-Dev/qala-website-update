ee3c138494001dc98582d30e1e7edb561feb2931
Oluwatobi Bamidele <teebams49@gmail.com>
2022-12-23 13:10:40 +0100

    update: removed a breaking line of code

--- pages/hire/fellows/[id]/index.tsx
+++ pages/hire/fellows/[id]/index.tsx
@@ -1,19 +1,15 @@
 import Details from "~/components/hirePages/fellowDetails";
 import { useRouter } from "next/router";
 import Head from "next/head";
-import FellowDatas from "../../../../fellows_data/data.json";
 
 export default function FellowDetails() {
   const router = useRouter();
-  const index: number = parseInt(router.query.id! as string);
-  const fellowData = FellowDatas[index];
   return (
     <>
       <Head>
-        <title>{fellowData.name}</title>
-        <meta name="description" content={fellowData.background} />
+        <title>Qala Fellow</title>
         <link rel="icon" href="/favicon.ico" />
-      </Head>{" "}
+      </Head>
       <Details fellowId={parseInt(router.query.id! as string)} />
     </>
   );
